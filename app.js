d7e7199afa3d8a5c93c5e69918b9ffd587b3fe67
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-24 12:11:07 -0400

    get sort by rating working

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ const app = express();
 app.use(morgan('common'));
 
 app.get('/apps', (req,res) => {
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  let filteredPlaystore = [...playstore];
+  const sortOptions = ['Rating','App'];
+  const sort = req.query.sort.toLowerCase();
+  const capitalSort = capitalizeFirstLetter(sort);
+  if(capitalSort && sortOptions.includes(capitalSort)) {
+    filteredPlaystore = filteredPlaystore.sort((a,b) => {
+      return a[capitalSort] < b[capitalSort] ? 1 : -1;
+    });
+  }
   console.log('/apps loaded');
-  res.json(playstore);
+  res.json(filteredPlaystore);
 });
 
 app.listen(8000, () => {
